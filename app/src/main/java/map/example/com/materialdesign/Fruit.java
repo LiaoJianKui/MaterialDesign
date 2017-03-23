ff50c64b9fc7d50f280923937f9ab5ff86c8dc16
HeiMaGirlDemo2 <1710869761@qq.com>
2017-03-23 14:37:04 +0800

    MaterialDesignDemo

---
+++ app/src/main/java/map/example/com/materialdesign/Fruit.java
@@ -0,0 +1,20 @@
+package map.example.com.materialdesign;
+
+/**
+ * Created by Administrator on 2017/3/22.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+    public Fruit(String name,int imageId){
+        this.name=name;
+        this.imageId=imageId;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getImageId(){
+        return imageId;
+    }
+}
